8d05049f29f541c038166a157cb5a01536614117
Dustin <adustin70@gmail.com>
2021-03-23 15:16:19 -0400

    Added carbon footprint map to home index

---
+++ CarbonProject/ApiKeys.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarbonProject
+{
+    public static class ApiKeys
+    {
+        public static string MapBoxPublishableKey
+        {
+            get
+            {
+                return ;
+            }
+        }
+    }
+}
