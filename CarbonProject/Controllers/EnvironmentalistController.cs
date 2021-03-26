60b7fea2aca5cecbb30e4166e807986498b90e21
Dustin <adustin70@gmail.com>
2021-03-26 09:23:03 -0400

    Added edit methods to EnvironmentalistController, Added links to edit views in Environmentalist view

--- CarbonProject/Controllers/EnvironmentalistController.cs
+++ CarbonProject/Controllers/EnvironmentalistController.cs
@@ -2,6 +2,7 @@
 using CarbonProject.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -28,7 +29,7 @@ namespace CarbonProject.Controllers
 
             if (user == null)
             {
-                return RedirectToAction("Create");
+                return RedirectToAction(nameof(Create));
             }
             else
             {
@@ -43,15 +44,43 @@ namespace CarbonProject.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create([Bind("Id, UserName, FirstName, LastName, State, IdentityUserId")]Environmentalist environmentalist)
+        public async Task<IActionResult> Create([Bind("Id, UserName, FirstName, LastName, State, IdentityUserId")]Environmentalist environmentalist)
         {
             if (ModelState.IsValid)
             {
                 var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
                 environmentalist.IdentityUserId = userId;
                 _context.Add(environmentalist);
-                _context.SaveChanges();
-                return RedirectToAction("Survey");
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Survey));
+            }
+            return View(environmentalist);
+        }
+
+        public IActionResult Edit()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = _context.Environmentalists.Where(e => e.IdentityUserId == userId).FirstOrDefault();
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit([Bind("Id, UserName, FirstName, LastName, State, IdentityUserId")] Environmentalist environmentalist)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var user = _context.Environmentalists.Where(e => e.IdentityUserId == userId).FirstOrDefault();
+
+                user.UserName = environmentalist.UserName;
+                user.FirstName = environmentalist.FirstName;
+                user.LastName = environmentalist.LastName;
+                user.State = environmentalist.State;
+
+                _context.Entry(user).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
             }
             return View(environmentalist);
         }
@@ -63,7 +92,7 @@ namespace CarbonProject.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Survey([Bind("Id, MilesDriven, FuelType, PlasticBagsUsed, PlasticBottlesUsed, PowerUsed, EnvironmentalistId")]Survey survey)
+        public async Task<IActionResult> Survey([Bind("Id, MilesDriven, FuelType, PlasticBagsUsed, PlasticBottlesUsed, PowerUsed, EnvironmentalistId")]Survey survey)
         {
             if (ModelState.IsValid)
             {
@@ -71,14 +100,45 @@ namespace CarbonProject.Controllers
                 var user = _context.Environmentalists.Where(e => e.IdentityUserId == userId).FirstOrDefault();
                 survey.EnvironmentalistId = user.Id;                
                 _context.Add(survey);
-                _context.SaveChanges();
+                await _context.SaveChangesAsync();
                 CarbonData(survey, survey.FuelType);
                 return RedirectToAction(nameof(Index));
             }
             return View(survey);
         }
 
-        public void CarbonData(Survey survey, string fuelType)
+        public IActionResult EditSurvey()
+        {
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = _context.Environmentalists.Where(e => e.IdentityUserId == userId).FirstOrDefault();
+            var survey = _context.Surveys.Where(s => s.EnvironmentalistId == user.Id).FirstOrDefault();
+            return View(survey);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditSurvey(Survey survey)
+        {
+            if (ModelState.IsValid)
+            {
+                var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+                var user = _context.Environmentalists.Where(e => e.IdentityUserId == userId).FirstOrDefault();
+                var chosenSurvey = _context.Surveys.Where(s => s.EnvironmentalistId == user.Id).FirstOrDefault();
+
+                chosenSurvey.MilesDriven = survey.MilesDriven;
+                chosenSurvey.FuelType = survey.FuelType;
+                chosenSurvey.PlasticBagsUsed = survey.PlasticBagsUsed;
+                chosenSurvey.PlasticBottlesUsed = survey.PlasticBottlesUsed;
+                chosenSurvey.PowerUsed = survey.PowerUsed;
+
+                _context.Entry(chosenSurvey).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(survey);
+        }
+
+        public async Task<IActionResult> CarbonData(Survey survey, string fuelType)
         {
             var footprint = _context.CarbonFootprints.Where(f => f.FuelTypeData == fuelType).FirstOrDefault();
 
@@ -88,7 +148,8 @@ namespace CarbonProject.Controllers
             footprint.PowerUsedData = survey.PowerUsed;
 
             _context.Update(footprint);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
+            return Ok();
         }
 
         public IActionResult SerializeCarbonData(CarbonFootprint footprint)
